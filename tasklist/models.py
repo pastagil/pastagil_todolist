3998f073fc7840b75e142868773a35123b15fea6
Sebastian Fehlandt <sebafehlandt@gmail.com>
2017-08-17 22:20:49 +0000

    Test list tasks in models. Closes #6

--- tasklist/models.py
+++ tasklist/models.py
@@ -6,3 +6,6 @@ from django.db import models
 # Task model
 class Task(models.Model):
     description = models.CharField(max_length=200, blank=True, null=True)
+    
+    def __str__(self):
+        return self.description
--- tasklist/tests.py
+++ tasklist/tests.py
@@ -1,42 +1,61 @@
 from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
 from .models import Task
+from random import shuffle
 
 # Create your tests here.
 
+
 class TaskModelTest(TestCase):
+    
     
     def setUp(self):
         self.description = "afeitar el gato"
     
+    
     def testCreate(self):
         #given
         initial_count = Task.objects.all().count()
-        
         #when
         task = Task.objects.create(description=self.description)
-        
         #then
         final_count = Task.objects.all().count()
         task = Task.objects.get(description = self.description)
-        
         self.assertEqual(initial_count+1,final_count)
         self.assertEqual(task.description, self.description)
+        
         
     def testDelete(self):
         #given
         task = Task.objects.create(description=self.description)
         initial_count = Task.objects.all().count()
-        
         #when
         Task.objects.get(description=self.description).delete()
-        
         #then
         final_count = Task.objects.all().count()
         self.assertEqual(initial_count-1,final_count)
         self.assertEqual(Task.objects.filter(description=self.description).exists(), False)
+    
+    
+    def testList(self):
+        # given
+        task_list = [
+            Task(description = self.description+' 1'),
+            Task(description = self.description+' 2'),
+            Task(description = self.description+' 3'),
+        ]
+        for task in task_list:
+            task.save()
+        # when
+        retrieved_tasks = list(Task.objects.all())
+        retrieved_tasks.reverse()
+        # then
+        for task in task_list:
+            self.assertTrue(task in retrieved_tasks)
+
 
 class TaskInterfaceTest(TestCase):
+    
     
     def testInterface(self):
         # given:
