02051ea8017fa2da6fd58bd8a2902f8f7ac205c7
Sebastian Fehlandt <sebafehlandt@gmail.com>
2017-08-17 20:25:32 +0000

    comentario

--- tasklist/models.py
+++ tasklist/models.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-
+# Task model
 class Task(models.Model):
     description = models.CharField(max_length=200, blank=True, null=True)
