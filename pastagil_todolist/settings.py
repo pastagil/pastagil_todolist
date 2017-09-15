0a2d3ed40ee5d9a611e186377c8b57e961f7e029
kloness <pablog.acc@gmail.com>
2017-09-15 00:48:58 -0300

    static files setup

--- pastagil_todolist/settings.py
+++ pastagil_todolist/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static_files'),)
+
 
 # Heroku config
 import dj_database_url
